32884d13b690a2343705529f14e77caf29192d5c
Teemu Koivisto <tkoivisto456@gmail.com>
2017-11-12 13:08:16 +0200

    Fix specify python version using pip

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,12 @@ fi
 # Copy the build into bucket and delete all existing files
 aws s3 sync ./build s3://kurssim.me \
   --region eu-central-1 \
-  --acl bucket-owner-full-control \
+  --acl public-read \
   --cache-control max-age=2592000 \
   --delete
 
 # Copy index.html the second time with decreased cache duration
 aws s3 cp ./build/index.html s3://kurssim.me \
   --region eu-central-1 \
-  --acl bucket-owner-full-control \
+  --acl public-read \
   --cache-control max-age=120
