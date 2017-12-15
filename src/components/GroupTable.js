fb562c3093f0256b56471d945711caba3996ed7f
Teemu Koivisto <tkoivisto456@gmail.com>
2017-12-16 00:03:24 +0200

    Enhance GroupTable styles

--- src/components/GroupTable.js
+++ src/components/GroupTable.js
@@ -2,19 +2,6 @@ import React from 'react'
 
 import './GroupTable.scss'
 
-const groupTimeClass = (index) => {
-  switch(index) {
-    case 0:
-      return 'date'
-    case 1:
-      return 'day'
-    case 2:
-      return 'time'
-    default:
-      return ''
-  }
-}
-
 const GroupTable = ({ groups }) => (
   <div className="group-table">
     <div className="group-table__header">
@@ -22,8 +9,7 @@ const GroupTable = ({ groups }) => (
       <div className="group__teacher">Vetäjä</div>
       <div className="group__enrolled">Ilm.</div>
       <div className="group__maximum">Max</div>
-      <div className="group__time">Pvm</div>
-      <div className="group__classroom">Luokka</div>
+      <div className="group__schedule__header">Aikataulu</div>
     </div>
     {groups.map((group, i) => (
       <div key={i} className="group-table__item">
@@ -32,7 +18,7 @@ const GroupTable = ({ groups }) => (
           <div className="group__teacher">Vetäjä</div>
           <div className="group__enrolled">Ilm.</div>
           <div className="group__maximum">Max</div>
-          <div className="group__time">Ryhmät</div>
+          <div className="group__schedule__header">Aikataulu</div>
         </div>
         <div className="group-table__item__body">
           <div className="group__name">{group.group_name}</div>
@@ -48,17 +34,16 @@ const GroupTable = ({ groups }) => (
           <div className="group__schedule__container">
             {group.schedule.map((s, i) => (
               <div key={i} className="group__schedule">
-                <div className="group__time--full">
-                  {s.time.split(' ').map((timeChunk, i) => (
-                    <div
-                      key={i}
-                      className={`group__time__${groupTimeClass(i)}`}
-                    >
-                      {timeChunk}
-                    </div>
-                  ))}
+                <div className="schedule__day">
+                  {s.day}
                 </div>
-                <div className="group__classroom--full">
+                <div className="schedule__date">
+                  {s.date}
+                </div>
+                <div className="schedule__time">
+                  {s.time}
+                </div>
+                <div className="schedule__classroom">
                   {s.classroom}
                 </div>
               </div>
